dad5601810cba202d81e26e9170cd43678915975
abdy254 <abdymohammed@gmail.com>
2021-03-05 17:23:11 +0300

    Add Bootstrap base template

--- vaccination_registry/views.py
+++ vaccination_registry/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Person
+from django.contrib.auth.models import User
 
 #test view
 
 def test(request):
-    return render(request, 'index.html')
+    return render(request, 'sidebar.html')
